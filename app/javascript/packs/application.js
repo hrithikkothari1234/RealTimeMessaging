6729b5b66b88f89a10696de860da17c85c587fe8
hritikkothari <kotharihrithik1@gmail.com>
2021-06-27 15:17:26 +0530

    fixed jquery bug, added favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require semantic-ui
 //= require jquery
-
+//= require semantic-ui
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready(function() {
+  $(`.ui.dropdown`).dropdown()
+});
